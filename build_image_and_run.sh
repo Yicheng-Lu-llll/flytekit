81e3d6378a5d176282b01ab216e537bb6ae70d4b
Yicheng-Lu-llll <luyc58576@gmail.com>
2023-04-07 00:42:32 +0000

    test
    Signed-off-by: Yicheng-Lu-llll <luyc58576@gmail.com>

--- build_image_and_run.sh
+++ build_image_and_run.sh
@@ -5,9 +5,10 @@ export KUBECONFIG=$KUBECONFIG:/root/.kube/config:/root/.flyte/k3s/k3s.yaml
 
 export FLYTE_INTERNAL_IMAGE="yichenglu/flytekit:test_visualization"
 
+git add . && git commit -s -m "test" && git push origin testvisualization
+
 docker build --no-cache -t  "${FLYTE_INTERNAL_IMAGE}" .
 docker push ${FLYTE_INTERNAL_IMAGE}
 pyflyte run --image ${FLYTE_INTERNAL_IMAGE} --remote ./test.py  wf
 
 
-git add . && git commit -s -m "test" && git push origin testvisualization
