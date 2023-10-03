547440de0d2bc13434d42320a247fb842c4cdaea
Yicheng-Lu-llll <luyc58576@gmail.com>
2023-10-03 00:17:38 +0000

    test
    Signed-off-by: Yicheng-Lu-llll <luyc58576@gmail.com>

--- plugins/flytekit-ray/flytekitplugins/ray/agent.py
+++ plugins/flytekit-ray/flytekitplugins/ray/agent.py
@@ -1,38 +1,42 @@
-from flytekit.extend.backend.base_agent import AgentBase, AgentRegistry
+import json
+import pickle
+import typing
 from dataclasses import dataclass
-import requests
+from typing import Optional
+
+
+import flytekit
+from flytekit.extend.backend.base_agent import AgentBase, AgentRegistry, convert_to_flyte_state
+from flytekit.models.literals import LiteralMap
+from flytekit.models.task import TaskTemplate
+
 
 @dataclass
 class Metadata:
-    # you can add any metadata you want, propeller will pass the metadata to the agent to get the job status.
-    # For example, you can add the job_id to the metadata, and the agent will use the job_id to get the job status.
-    # You could also add the s3 file path, and the agent can check if the file exists.
-    job_id: str
+
 
 class RayAgent(AgentBase):
-    def __init__(self, task_type: str):
-        # Each agent should have a unique task type. Agent service will use the task type to find the corresponding agent.
-        super().__init__(task_type="ray")
-        print("RayAgent")
+    def __init__(self):
+        super().__init__(task_type="spark")
 
     async def async_create(
         self,
         context: grpc.ServicerContext,
         output_prefix: str,
         task_template: TaskTemplate,
-        inputs: typing.Optional[LiteralMap] = None,
-    ) -> TaskCreateResponse:
-        print(__name__)
+        inputs: Optional[LiteralMap] = None,
+    ) -> CreateTaskResponse:
 
         return CreateTaskResponse()
 
-    async def async_get(self, context: grpc.ServicerContext, resource_meta: bytes) -> TaskGetResponse:
+    async def async_get(self, context: grpc.ServicerContext, resource_meta: bytes) -> GetTaskResponse:
 
         return GetTaskResponse()
 
-    async def async_delete(self, context: grpc.ServicerContext, resource_meta: bytes) -> TaskDeleteResponse:
+    async def async_delete(self, context: grpc.ServicerContext, resource_meta: bytes) -> DeleteTaskResponse:
+
         return DeleteTaskResponse()
 
-# To register the custom agent
-print("register!!!!!!!!!!!!!")
+
+
 AgentRegistry.register(RayAgent())
