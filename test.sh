e49880805ed76a30aaa2ac3145b5851810354863
Yicheng-Lu-llll <luyc58576@gmail.com>
2023-10-03 00:14:30 +0000

    test
    Signed-off-by: Yicheng-Lu-llll <luyc58576@gmail.com>

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@ git add .
 git commit -s -m "test"
 git push
 
+pip uninstall -y flytekit
+
 pip install -U git+https://github.com/Yicheng-Lu-llll/flytekit.git@"ray-agent#egg=flytekitplugins-ray&subdirectory=plugins/flytekit-ray" # replace with your own repo and branch
 pip install -U git+https://github.com/Yicheng-Lu-llll/flytekit.git@ray-agent
 
